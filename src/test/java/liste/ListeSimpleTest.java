14c22fc34f460be16cdeea9a4639bca90113f24d
Teo <teogi@NiceDeBrice.localdomain>
2026-09-17 14:57:20 +0200

    ex3,2,2

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -49,7 +49,7 @@ public class ListeSimpleTest {
         listeATester.ajout(1);
         listeATester.ajout(2);
         listeATester.ajout(3);
-        assertEquals("ListeSimple(3, 2, 1)", listeATester.toString());
+        assertEquals("<ListeSimple(Noeud(3), Noeud(2), Noeud(1))>", listeATester.toString());
     }
 
     @Test
